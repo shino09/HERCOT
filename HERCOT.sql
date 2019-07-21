a475a1f0b431ad69f0bf3ca2d3c64bf73f0b7ea8
shino09 <bambasten9@gmail.com>
2019-07-21 16:45:54 -0400

    es cript de la bd porsiacaso

--- HERCOT.sql
+++ HERCOT.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 19-07-2019 a las 22:38:28
+-- Tiempo de generación: 21-07-2019 a las 22:44:33
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 5.6.31
 
@@ -44,8 +44,10 @@ CREATE TABLE `appointments` (
 --
 
 INSERT INTO `appointments` (`id`, `date`, `price`, `dentist_id`, `patient_id`, `service_id`, `created_at`, `updated_at`) VALUES
-(3, '16-07-2019', 222, 2, 13, 1, '2019-07-19 20:32:53', '2019-07-20 00:30:34'),
-(4, '31-07-2019', 11111111, 2, 11, 1, '2019-07-19 20:45:09', '2019-07-19 21:26:56');
+(3, '16-07-2019', 222, 3, 13, 1, '2019-07-19 20:32:53', '2019-07-21 23:01:56'),
+(4, '31-07-2019', 11111111, 2, 14, 1, '2019-07-19 20:45:09', '2019-07-21 18:49:29'),
+(5, '24-07-2019', 6456, 4, 10, 3, '2019-07-20 01:02:09', '2019-07-21 23:02:14'),
+(7, '10-07-2019', 434334, 3, 15, 3, '2019-07-21 23:50:00', '2019-07-21 23:50:00');
 
 -- --------------------------------------------------------
 
@@ -65,7 +67,9 @@ CREATE TABLE `dentists` (
 --
 
 INSERT INTO `dentists` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(2, 'DFFDFD', '2019-07-19 18:52:41', '2019-07-19 18:52:41');
+(2, 'rr hh de hercot', '2019-07-21 18:41:47', '2019-07-21 22:41:47'),
+(3, 'juan', '2019-07-21 19:00:49', '2019-07-21 23:00:49'),
+(4, 'pedro', '2019-07-21 23:00:41', '2019-07-21 23:00:41');
 
 -- --------------------------------------------------------
 
@@ -87,8 +91,9 @@ CREATE TABLE `patients` (
 INSERT INTO `patients` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (10, 'hola', '2019-07-19 16:53:06', '2019-07-19 16:53:06'),
 (11, 'sdsdffddf', '2019-07-19 17:23:42', '2019-07-19 17:23:42'),
-(13, 'hola254545454', '2019-07-19 17:32:41', '2019-07-19 18:26:50'),
-(14, 'timoteo', '2019-07-19 17:32:50', '2019-07-19 18:37:17');
+(13, 'juan perez', '2019-07-19 17:32:41', '2019-07-21 23:00:26'),
+(14, 'timoteo', '2019-07-19 17:32:50', '2019-07-19 18:37:17'),
+(15, 'mandinga', '2019-07-21 23:00:17', '2019-07-21 23:00:17');
 
 -- --------------------------------------------------------
 
@@ -109,7 +114,9 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`id`, `name`, `price`, `created_at`, `updated_at`) VALUES
-(1, 'dental', 3443, '2019-07-19 18:58:12', '2019-07-19 19:13:09');
+(1, 'dental', 3443, '2019-07-19 18:58:12', '2019-07-19 19:13:09'),
+(2, 'rayos x', 22, '2019-07-21 22:41:21', '2019-07-21 22:41:21'),
+(3, 'ortodoncia', 133322, '2019-07-21 23:01:04', '2019-07-21 23:01:10');
 
 --
 -- Índices para tablas volcadas
@@ -150,22 +157,22 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT de la tabla `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT de la tabla `dentists`
 --
 ALTER TABLE `dentists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
